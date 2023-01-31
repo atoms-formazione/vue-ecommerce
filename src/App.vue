030ad196eda75e86e5412e77553c5c604fbe3ccb
Pietro Stivanello <stivanellop@yahoo.com>
2023-01-31 10:38:24 +0100

    navbar

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
+  <Navbar></Navbar>
+
   <header>HEADER</header>
 
   <RouterView />
