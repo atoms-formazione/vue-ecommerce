f674adbe60e24467aab87928e8f62ad313b1a777
Daniele Ghilardini <ghilardinidaniele@gmail.com>
2023-01-31 12:12:47 +0100

    Completed Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
-  <header>HEADER</header>
+  <header>
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Manrope:wght@200;300;400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+  </header>
 
-  <RouterView />
+  <Navbar />
 </template>
+
+<style>
+* {
+  font-family: "Manrope", sans-serif;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import TopLinks from "./TopLinks.vue";
+import LoginRegister from "./LoginRegister.vue";
+</script>
+
+<template>
+  <div class="navbar">
+    <h1 class="logo">
+      <span class="black">Ba</span><span class="green">chira</span>
+    </h1>
+    <TopLinks active="home" class="toplinks" />
+    <LoginRegister class="loginregister" />
+  </div>
+</template>
+
+<style>
+.navbar {
+  display: flex;
+  gap: 5rem;
+}
+
+.logo {
+  font-weight: 700;
+  size: 36px;
+  flex: 0.3;
+  margin-left: 20px;
+}
+
+.black {
+  color: #101913;
+}
+
+.green {
+  color: #3c9379;
+}
+
+.toplinks {
+  flex: 1;
+}
+
+.loginregister {
+  flex: 0.1;
+  margin-right: 20px;
+}
+</style>
