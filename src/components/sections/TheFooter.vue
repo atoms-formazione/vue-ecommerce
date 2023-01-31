eba5d569ffeadd118b1beb2ef50234c802c1c85d
Alessandro Tecchio <tecchio.alessandro.96@gmail.com>
2023-01-31 16:23:52 +0100

    feat: footer initial setup

---
+++ src/components/sections/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="footer"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.footer {
+  background-color: var(--black);
+  color: var(--white);
+  max-height: 6rem;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
+</style>
