b5fd0a6b52bfd9e583b290a7b4274867c3078978
Martin Masevski <martin.masevski@gmail.com>
2023-02-05 19:26:33 +0100

    feat: add navbar

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const links = reactive([
+  { name: 'Home', isActive: true },
+  { name: 'Products' },
+  { name: 'Categories' },
+  { name: 'Brands' },
+  { name: 'Pricing' },
+])
+</script>
 
 <template>
-  <header>
-    <IconsLogoBachira />
+  <!-- TODO: remove static padding -->
+  <header class="flex justify-between px-10 pt-10 items-center">
+    <a href="/">
+      <IconsLogoBachira />
+    </a>
+
+    <nav>
+      <ul class="flex gap-10">
+        <li v-for="{ name, isActive } in links" :key="name" class="text-grey" :class="{ 'text-black': isActive }">
+          <a href="/">{{ name }}</a>
+        </li>
+      </ul>
+    </nav>
+
+    <div class="flex gap-5">
+      <button>Login</button>
+      <button
+        class="py-3 px-[18px] rounded-lg bg-green text-white
+      "
+      >
+        Register
+      </button>
+    </div>
   </header>
 </template>
