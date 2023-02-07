52f09f5659ab52b5df066c4f0bd344bea03bffd6
Moneta02 <pietro.coin02@gmail.com>
2023-02-07 09:14:55 +0100

    add api

--- src/components/atoms/ProductCard.vue
+++ src/components/atoms/ProductCard.vue
@@ -1,43 +1,33 @@
+S
 <template>
   <div class=" ">
-    <h2 class="">
-      Populer Products From
-      All Brands
-    </h2>
-    <div class="">
-      <div v-for="{ id, thumbnail, title, price, brand } in products" :key="id">
+    <h2 class="">Populer Products From All Brands</h2>
+    <div v-if="response">
+      <div v-for="product in response.products" :key="product.id">
         <div class="">
-          <div
-            class=""
-          >
-            <img :src="thumbnail" :alt="title" class="">
+          <div class="">
+            <img :src="product.thumbnail" :alt="product.title" class="" />
           </div>
         </div>
-        <h3 class="">
-          {{ title }} - {{ brand }}
-        </h3>
-        <h4>{{ price }}$</h4>
+        <h3 class="">{{ product.title }} - {{ product.brand }}</h3>
+        <h4>{{ product.price }}$</h4>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const products = ref([]);
-
-await fetch('https://dummyjson.com/products/?limit=12')
-  .then(res => res.json())
-  .then(data => products.value = data.products)
+const response = await fetch("https://dummyjson.com/products?limit=10").then(
+  (res) => res.json()
+);
 </script>
 
 <style scoped>
-.card{
-
+.card {
 }
 
 .image {
-    width: 499px;
-    height: 399px;
-
+  width: 499px;
+  height: 399px;
 }
 </style>
